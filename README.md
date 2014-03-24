ffc33321e693f2c651d669f29b5de356678c19bc
zhenjing <zhenjinglee@gmail.com>
2014-03-24 11:03:28 +0800

    add template and update README

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ proupy
 proupy is a web framework for Python that is as simple as it is powerful.
 
 It does not realize template module and session module, it is very suitable to write API.
+
+It is based web.py_ achieved, but does not realize the session module, because session can be achieved through other methods.
+
+.. _web.py: https://github.com/webpy/webpy
--- proupy/utils.py
+++ proupy/utils.py
@@ -37,6 +37,7 @@ class Storage(dict):
 
 	def __repr__(self):     
 		return '<Storage ' + dict.__repr__(self) + '>'
+storage = Storage
 
 def safestr(obj, encoding='utf-8'):
 	if isinstance(obj, unicode):
@@ -45,3 +46,74 @@ def safestr(obj, encoding='utf-8'):
 		return obj
 	else:
 		return str(obj)
+
+def htmlquote(text):
+	r"""
+	Encodes `text` for raw use in HTML.
+
+		>>> htmlquote(u"<'&\">")
+		u'&lt;&#39;&amp;&quot;&gt;'
+	"""
+	text = text.replace(u"&", u"&amp;") # Must be done first!
+	text = text.replace(u"<", u"&lt;")
+	text = text.replace(u">", u"&gt;")
+	text = text.replace(u"'", u"&#39;")
+	text = text.replace(u'"', u"&quot;")
+	return text
+
+def htmlunquote(text):
+	r"""
+	Decodes `text` that's HTML quoted.
+
+		>>> htmlunquote(u'&lt;&#39;&amp;&quot;&gt;')
+		u'<\'&">'
+	"""
+	text = text.replace(u"&quot;", u'"')
+	text = text.replace(u"&#39;", u"'")
+	text = text.replace(u"&gt;", u">")
+	text = text.replace(u"&lt;", u"<")
+	text = text.replace(u"&amp;", u"&") # Must be done last!
+	return text
+
+def websafe(val):
+	r"""Converts `val` so that it is safe for use in Unicode HTML.
+
+		>>> websafe("<'&\">")
+		u'&lt;&#39;&amp;&quot;&gt;'
+		>>> websafe(None)
+		u''
+		>>> websafe(u'\u203d')
+		u'\u203d'
+		>>> websafe('\xe2\x80\xbd')
+		u'\u203d'
+	"""
+	if val is None:
+		return u''
+	elif isinstance(val, str):
+		val = val.decode('utf-8')
+	elif not isinstance(val, unicode):
+		val = unicode(val)
+	return htmlquote(val)
+
+def safeunicode(obj, encoding='utf-8'):
+	r"""
+	Converts any given object to unicode string.
+	
+		>>> safeunicode('hello')
+		u'hello'
+		>>> safeunicode(2)
+		u'2'
+		>>> safeunicode('\xe1\x88\xb4')
+		u'\u1234'
+	"""
+	t = type(obj)
+	if t is unicode:
+		return obj
+	elif t is str:
+		return obj.decode(encoding)
+	elif t in [int, float, bool]:
+		return unicode(obj)
+	elif hasattr(obj, '__unicode__') or isinstance(obj, unicode):
+		return unicode(obj)
+	else:
+		return str(obj).decode(encoding)
