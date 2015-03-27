a8334285fd9627adeffceb85b7a72421c416b279
zhenjing <zhenjinglee@gmail.com>
2015-03-27 14:32:25 +0800

    replace tab to space

--- examples/hello.py
+++ examples/hello.py
@@ -9,29 +9,29 @@ import proupy.web
 import proupy.wsgi
 
 class Index(proupy.web.HTTPRequest):
-	def GET(self):
-		return "Hello word"
+    def GET(self):
+        return "Hello word"
 
 class Test(proupy.web.HTTPRequest):
-	def GET(self, arg):
-		return "Hello %s" % arg
+    def GET(self, arg):
+        return "Hello %s" % arg
 
 url = [("/", Index),
-		("/(.*)", Test)]
+        ("/(.*)", Test)]
 
 
 application = proupy.wsgi.Application(url)
 
 
 if __name__ == "__main__":
-	from gevent import monkey;monkey.patch_all()
-	from gevent.pywsgi import WSGIServer
-	if len(sys.argv) > 1:
-		if sys.argv[1].isdigit():
-			print("http://localhost:"+sys.argv[1])
-			WSGIServer(("" ,int(sys.argv[1])),application).serve_forever()
-		else:
-			print("invalid parameters")
-	else:
-		print("http://localhost:8080")
-		WSGIServer(("" , 8080),application).serve_forever()
+    from gevent import monkey;monkey.patch_all()
+    from gevent.pywsgi import WSGIServer
+    if len(sys.argv) > 1:
+        if sys.argv[1].isdigit():
+            print("http://localhost:"+sys.argv[1])
+            WSGIServer(("" ,int(sys.argv[1])),application).serve_forever()
+        else:
+            print("invalid parameters")
+    else:
+        print("http://localhost:8080")
+        WSGIServer(("" , 8080),application).serve_forever()
--- examples/template.py
+++ examples/template.py
@@ -9,8 +9,8 @@ import proupy.web
 import proupy.wsgi
 
 class Index(proupy.web.HTTPRequest):
-	def GET(self):
-		return self.render("index")
+    def GET(self):
+        return self.render("index")
 
 url = [("/", Index)]
 
@@ -19,14 +19,14 @@ application = proupy.wsgi.Application(url, {"template_path":"templates"})
 
 
 if __name__ == "__main__":
-	from gevent import monkey;monkey.patch_all()
-	from gevent.pywsgi import WSGIServer
-	if len(sys.argv) > 1:
-		if sys.argv[1].isdigit():
-			print("http://localhost:"+sys.argv[1])
-			WSGIServer(("" ,int(sys.argv[1])),application).serve_forever()
-		else:
-			print("invalid parameters")
-	else:
-		print("http://localhost:8080")
-		WSGIServer(("" , 8080),application).serve_forever()
+    from gevent import monkey;monkey.patch_all()
+    from gevent.pywsgi import WSGIServer
+    if len(sys.argv) > 1:
+        if sys.argv[1].isdigit():
+            print("http://localhost:"+sys.argv[1])
+            WSGIServer(("" ,int(sys.argv[1])),application).serve_forever()
+        else:
+            print("invalid parameters")
+    else:
+        print("http://localhost:8080")
+        WSGIServer(("" , 8080),application).serve_forever()
